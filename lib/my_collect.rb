0f572413c7c64a272c4ffdff3e4393ace5869948
Yingqi Chen <chanwk18@hotmail.com>
2019-03-25 19:50:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,5 @@
 def my_collect(array)
+<<<<<<< HEAD
   if block_given?
     i=0
     array2=[]
@@ -7,6 +8,13 @@ def my_collect(array)
       i=i+1
     end
   else puts " NO BLOCK!!"
+=======
+    i=0
+    array2=[]
+    while i<array.length
+      array2 << yield (array[i])
+      i=i+1
+>>>>>>> ac8e6a89960e4825b93779284e75ae8e458500d4
   end
   array2
 end
